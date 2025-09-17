b8cac2253e433a7379d59aefc2cffe48200a4b04
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-09-16 20:35:42 -0500

    bugs fixed

--- dia4/Fruit/src/Main.java
+++ dia4/Fruit/src/Main.java
@@ -6,7 +6,7 @@ import service.FruitService;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        FruitService.addFruit(new Fruit("banana", 80, "yellow", 1000, true, false));
+        FruitService.addFruit(new Fruit("banana", 0.1, "yellow", 1000, true, false));
 
         MenuController.welcomeMessage();
         MenuController.showMenu();
