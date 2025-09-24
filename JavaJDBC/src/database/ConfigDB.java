f50c91dd219061b312d133a2709bc11218b291c8
osvaldocs <100969798+osvaldocs@users.noreply.github.com>
2025-09-24 13:25:55 -0500

    Update ConfigDB.java

--- JavaJDBC/src/database/ConfigDB.java
+++ JavaJDBC/src/database/ConfigDB.java
@@ -10,9 +10,9 @@ public class ConfigDB {
     public static Connection openConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/user_manager";
+            String url = "jdbc:mysql://localhost:3306/user_manager"; //nombre de la base de datos mysql
             String user = "root";
-            String password = "Qwe.123*";
+            String password = "Qwe.123*"; // contraseña que tenga en mysql
 
             objConnection = (Connection) DriverManager.getConnection(url, user, password);
             System.out.println("Conexion exitosa");
