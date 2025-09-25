168dc434b6338c4ba1a329c6e33b909312238ec1
osvaldocs <100969798+osvaldocs@users.noreply.github.com>
2025-09-25 13:43:32 -0500

    Delete JavaJDBC directory

--- JavaJDBC/src/Main.java
+++
@@ -1,9 +0,0 @@
-import database.ConfigDB;
-
-import java.sql.Connection;
-
-public class Main {
-    public static void main(String[] args) {
-        Connection connection = ConfigDB.openConnection();
-    }
-}
